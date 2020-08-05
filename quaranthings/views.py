1c5009d28470e266d3d380b70dbe40eb87504676
Ben Tsai <benjamintsa@gmail.com>
2020-08-05 15:02:22 -0700

    added landing page for listing form if user not logged in

--- quaranthings/views.py
+++ quaranthings/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def new_quaranthing(request):
-    if 'logged_user' not in request.session:
-        return redirect('/')
-    else:
-        return render(request, 'new_quaranthing.html')
+    return render(request, 'new_quaranthing.html')
