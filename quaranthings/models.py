64049775ca4756e4d6c9c2baab558f846b31b958
Ben Tsai <benjamintsa@gmail.com>
2020-08-06 15:36:53 -0700

    added categories, category toggle on quaranthing listing form

--- quaranthings/models.py
+++ quaranthings/models.py
@@ -25,4 +25,20 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2) #allow for items to be priced no more than $9999.99
     stock = models.IntegerField() #how many of this product is in stock
     seller = models.ForeignKey(User, related_name = "products_listed", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     objects = ProductManager()
+
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+    category_type = models.CharField(max_length = 40)
+    products = models.ManyToManyField(Product, related_name="categories")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Image(models.Model):
+    image = models.FileField()
+    caption = models.TextField(blank=True)
+    products = models.ManyToManyField(Product, related_name="images")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- quaranthings/views.py
+++ quaranthings/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, redirect
+from .models import *
+from login.models import *
+from django.contrib import messages
 
 # Create your views here.
 
 def new_quaranthing(request):
-    return render(request, 'new_quaranthing.html')
+    context = {
+        'activity_categories':Category.objects.filter(category_type = 'activity').all(),
+        'diy_categories': Category.objects.filter(category_type = 'DIY Item').all()
+    }
+    return render(request, 'new_quaranthing.html', context)
+
+def process_new_quaranthing(request):
+    if request.method == "GET":
+        redirect('/quaranthings/new')
+    else:
+        errors = Product.objects.validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            redirect('/quaranthings/new')
+        else:
+            Product.objects.create()
